2a94a09932a215ea75df9e35f866265736501497
Joshua Rodgers <bytemr@gmail.com>
2020-07-12 23:05:36 -0700

    Add basic window setup code.

--- worp-client/src/main.rs
+++ worp-client/src/main.rs
@@ -1,2 +1,23 @@
-fn main() {
+use anyhow::Result;
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
+fn main() -> Result<()> {
+    let event_loop = EventLoop::new();
+    let _window = WindowBuilder::new().with_title("WORP").build(&event_loop)?;
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Poll;
+
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => *control_flow = ControlFlow::Exit,
+            _ => (),
+        }
+    });
 }
