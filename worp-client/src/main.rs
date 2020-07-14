1edeb111ac0ebb385993212c680fe619cb4704aa
Joshua Rodgers <bytemr@gmail.com>
2020-07-13 19:23:59 -0700

    Start working on the worp-renderer crate to handle rendering for the application.

--- worp-client/src/main.rs
+++ worp-client/src/main.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use tracing::{info, Level};
+use tracing::{error, info, Level};
 use tracing_subscriber::FmtSubscriber;
 use winit::{
     event::{Event, WindowEvent},
@@ -11,20 +11,36 @@ fn main() -> Result<()> {
     FmtSubscriber::builder().with_max_level(Level::INFO).init();
 
     let event_loop = EventLoop::new();
-    let _window = WindowBuilder::new().with_title("WORP").build(&event_loop)?;
+    // TODO: Initialize window with config settings.
+    let window = WindowBuilder::new().with_title("WORP").with_visible(false).build(&event_loop)?;
+
+    let mut renderer = futures_executor::block_on(renderer::Renderer::new(&window))?;
+
+    window.set_visible(true);
 
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Poll;
-
         match event {
+            Event::WindowEvent {
+                event: WindowEvent::Resized(size),
+                ..
+            } => {
+                renderer.resize(size.width, size.height);
+            }
+            Event::RedrawRequested(_) => {
+                if let Err(error) = renderer.draw_frame() {
+                    error!("Error drawing frame: {}", error);
+                    *control_flow = ControlFlow::Exit;
+                }
+            }
             Event::WindowEvent {
                 event: WindowEvent::CloseRequested,
                 ..
             } => {
                 *control_flow = ControlFlow::Exit;
-                info!("Closing application.");
+                info!("Closing application.")
             }
-            _ => (),
+            _ => {}
         }
     });
 }
--- worp-renderer/src/lib.rs
+++ worp-renderer/src/lib.rs
@@ -1,7 +1,95 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use anyhow::{Context as _, Result};
+use wgpu::{Adapter, Device, Instance, Queue, Surface, SwapChain, SwapChainDescriptor};
+use winit::window::Window;
+
+pub struct Renderer {
+    _instance: Instance,
+    window_surface: Surface,
+    _adapter: Adapter,
+    device: Device,
+    queue: Queue,
+    swap_chain_descriptor: SwapChainDescriptor,
+    swap_chain: SwapChain,
+}
+
+impl Renderer {
+    pub async fn new(window: &Window) -> Result<Self> {
+        let size = window.inner_size();
+        let instance = wgpu::Instance::new(wgpu::BackendBit::PRIMARY);
+        let window_surface = unsafe { instance.create_surface(window) };
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::Default,
+                compatible_surface: Some(&window_surface),
+            })
+            .await
+            .context("Failed to find appropriate adapter.")?;
+
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    features: wgpu::Features::empty(),
+                    limits: wgpu::Limits::default(),
+                    shader_validation: true,
+                },
+                None,
+            )
+            .await
+            .map_err(|_| RequestDeviceError)?;
+
+        let swap_chain_descriptor = wgpu::SwapChainDescriptor {
+            usage: wgpu::TextureUsage::OUTPUT_ATTACHMENT,
+            format: wgpu::TextureFormat::Bgra8UnormSrgb,
+            width: size.width,
+            height: size.height,
+            present_mode: wgpu::PresentMode::Immediate,
+        };
+
+        let swap_chain = device.create_swap_chain(&window_surface, &swap_chain_descriptor);
+
+        Ok(Self {
+            _instance: instance,
+            window_surface,
+            _adapter: adapter,
+            device,
+            queue,
+            swap_chain_descriptor,
+            swap_chain,
+        })
+    }
+
+    pub fn resize(&mut self, width: u32, height: u32) {
+        self.swap_chain_descriptor.width = width;
+        self.swap_chain_descriptor.height = height;
+        self.swap_chain = self.device.create_swap_chain(&self.window_surface, &self.swap_chain_descriptor);
+    }
+
+    pub fn draw_frame(&mut self) -> Result<()> {
+        let frame = self.swap_chain.get_next_frame().map_err(|_| SwapChainError)?.output;
+        let mut encoder = self.device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+
+        let _ = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            color_attachments: &[wgpu::RenderPassColorAttachmentDescriptor {
+                attachment: &frame.view,
+                resolve_target: None,
+                ops: wgpu::Operations {
+                    load: wgpu::LoadOp::Clear(wgpu::Color::GREEN),
+                    store: true,
+                },
+            }],
+            depth_stencil_attachment: None,
+        });
+
+        self.queue.submit(Some(encoder.finish()));
+
+        Ok(())
     }
 }
+
+#[derive(thiserror::Error, Debug)]
+#[error("Failed to create device.")]
+pub struct RequestDeviceError;
+
+#[derive(thiserror::Error, Debug)]
+#[error("Failed to acquire next swap chain framebuffer.")]
+pub struct SwapChainError;
