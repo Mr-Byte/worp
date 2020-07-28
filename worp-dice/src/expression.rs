482ef4ac4af4c8e6bd47db1632b17daa5b8526df
Joshua Rodgers <bytemr@gmail.com>
2020-07-28 00:16:26 -0700

    Implement most of the expression parsing for the Dice language.

---
+++ worp-dice/src/expression.rs
@@ -0,0 +1,108 @@
+use std::fmt::Display;
+
+#[derive(Debug, Clone)]
+pub struct Symbol(pub String);
+
+#[derive(Debug, Clone)]
+pub enum Literal {
+    /// Integer values such as `-1`, `0`, `1`, etc
+    Integer(i32),
+    /// Floating point decimals such as `-1.0, `0.0`, `1.1`, etc
+    Float(f32),
+    /// String literals such as `"hello"`
+    String(String),
+    /// Boolean literals (true or false)
+    Boolean(bool),
+    /// Lists, such as `[ 1, x, 3 ]`
+    List(Vec<Expression>),
+}
+
+#[derive(Debug, Clone)]
+pub enum UnaryOperator {
+    Negate,
+    Not,
+}
+
+#[derive(Debug, Clone)]
+pub enum BinaryOperator {
+    // Operators are ordered and grouped by precedence.
+    /// The `d` operator (dice rolls)
+    DiceRoll,
+
+    /// The `*` operator
+    Multiply,
+    /// The `/` operator
+    Divide,
+    /// The `%` operator
+    Remainder,
+
+    /// The `+` operator
+    Add,
+    /// The `-` operator
+    Subtract,
+
+    /// The `=` operator
+    Equals,
+    /// The `/=` operator
+    NotEquals,
+    /// The `>` operator
+    GreaterThan,
+    /// The `>=` operator
+    GreaterThanOrEquals,
+    /// The `<` operator
+    LessThan,
+    /// The `<=` operator
+    LessThanOrEquals,
+
+    /// The `and` operator
+    LogicalAnd,
+
+    /// The `or` operator
+    LogicalOr,
+
+    /// The `??` operator (null-coalesce)
+    Coalesce,
+
+    /// The `;` operator (discard)
+    Discard,
+}
+
+#[derive(Debug, Clone)]
+pub enum RangeOperator {
+    Exclusive,
+    Inclusive,
+}
+
+#[derive(Debug, Clone)]
+pub enum FieldAccessType {
+    Direct,
+    Safe,
+}
+
+#[derive(Debug, Clone)]
+pub enum Expression {
+    Literal(Literal),
+    /// Symbols represent some named item (variables, etc) within an expression
+    Symbol(Symbol),
+
+    //Primary operators
+    /// Access to a field, (e.g. `x.y`)
+    FieldAccess(FieldAccessType, Box<Expression>, Box<Expression>), // TODO: Figure out if this should be expr -> ident, or expr -> expr
+    /// Function call (e.g. `y(1, 2)`
+    /// First part evaluates to a function, second part is the parameters
+    FunctionCall(Box<Expression>, Vec<Expression>),
+    /// Indexed access (e.g. `x.y[1]` or `y["x"]`)
+    Index(Box<Expression>, Box<Expression>),
+
+    // Operators
+    Unary(UnaryOperator, Box<Expression>),
+    Binary(BinaryOperator, Box<Expression>, Box<Expression>),
+    Range(RangeOperator, Box<Expression>, Box<Expression>),
+    Conditional(Box<Expression>, Box<Expression>, Option<Box<Expression>>),
+}
+
+impl Display for Expression {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        todo!()
+    }
+}
---
+++ worp-dice/src/parser.rs
@@ -0,0 +1,245 @@
+// Remove once the parser is used via a public export.
+#![allow(dead_code)]
+
+use crate::expression::{BinaryOperator, Expression, Literal, RangeOperator, Symbol, UnaryOperator};
+use nom::{
+    branch::alt,
+    bytes::complete::{tag, take_till},
+    character::complete::{alpha1, alphanumeric1, digit1},
+    character::complete::{char, space0},
+    combinator::{map, map_res, opt},
+    multi::{fold_many0, many0, separated_list0},
+    number::complete::float,
+    sequence::{delimited, pair, preceded, separated_pair, terminated, tuple},
+    IResult,
+};
+
+fn float_literal(input: &str) -> IResult<&str, Literal> {
+    map(delimited(space0, terminated(float, char('f')), space0), Literal::Float)(input)
+}
+
+fn int_literal(input: &str) -> IResult<&str, Literal> {
+    let int = map_res(
+        tuple((opt(alt((char('+'), char('-')))), digit1)),
+        |(sign, value): (Option<char>, &str)| match sign {
+            Some('-') => value.parse().map(|value: i32| -value),
+            _ => value.parse(),
+        },
+    );
+
+    map(delimited(space0, int, space0), Literal::Integer)(input)
+}
+
+fn boolean_literal(input: &str) -> IResult<&str, Literal> {
+    let bool = map(alt((tag("true"), tag("false"))), |value| match value {
+        "true" => true,
+        "false" => false,
+        _ => unreachable!(),
+    });
+
+    map(delimited(space0, bool, space0), Literal::Boolean)(input)
+}
+
+fn string_literal(input: &str) -> IResult<&str, Literal> {
+    map(delimited(char('"'), take_till(|c| c == '"'), char('"')), |value: &str| {
+        Literal::String(value.to_owned())
+    })(input)
+}
+
+fn list(input: &str) -> IResult<&str, Literal> {
+    map(
+        delimited(char('['), separated_list0(delimited(space0, char(','), space0), expression), char(']')),
+        Literal::List,
+    )(input)
+}
+
+fn literal(input: &str) -> IResult<&str, Expression> {
+    map(
+        alt((float_literal, int_literal, string_literal, boolean_literal, list)),
+        Expression::Literal,
+    )(input)
+}
+
+fn symbol(input: &str) -> IResult<&str, Expression> {
+    let symbol_start = alt((tag("_"), alpha1));
+    let symbol_remainder = many0(alt((tag("_"), alphanumeric1)));
+    let symbol = pair(symbol_start, symbol_remainder);
+
+    delimited(
+        space0,
+        map(symbol, |(start, rest): (&str, Vec<&str>)| {
+            let mut result = String::new();
+            result += start;
+
+            for parts in &rest {
+                result += parts;
+            }
+
+            Expression::Symbol(Symbol(result))
+        }),
+        space0,
+    )(input)
+}
+
+fn parens(input: &str) -> IResult<&str, Expression> {
+    delimited(space0, delimited(char('('), expression, char(')')), space0)(input)
+}
+
+fn primary(input: &str) -> IResult<&str, Expression> {
+    alt((symbol, literal, parens))(input)
+}
+
+// TODO: Fix the bug here where unaries don't seem to parse correctly in scenarios like `!true && !true`
+fn unary(input: &str) -> IResult<&str, Expression> {
+    let unary_rule = map(pair(alt((char('-'), char('!'))), unary), |(op, expr)| match op {
+        '-' => Expression::Unary(UnaryOperator::Negate, Box::new(expr)),
+        '!' => Expression::Unary(UnaryOperator::Not, Box::new(expr)),
+        _ => unreachable!(),
+    });
+
+    alt((unary_rule, primary))(input)
+}
+
+fn dice_roll(input: &str) -> IResult<&str, Expression> {
+    alt((
+        map(separated_pair(unary, tag("d"), unary), |(lhs, rhs)| {
+            Expression::Binary(BinaryOperator::DiceRoll, Box::new(lhs), Box::new(rhs))
+        }),
+        unary,
+    ))(input)
+}
+
+fn multiplicative(input: &str) -> IResult<&str, Expression> {
+    let (input, init) = dice_roll(input)?;
+
+    fold_many0(pair(alt((char('*'), char('/'), char('%'))), dice_roll), init, |acc, (op, value)| {
+        let op = match op {
+            '*' => BinaryOperator::Multiply,
+            '/' => BinaryOperator::Divide,
+            '%' => BinaryOperator::Remainder,
+            _ => unreachable!(), // TODO: make this an error?
+        };
+
+        Expression::Binary(op, Box::new(acc), Box::new(value))
+    })(input)
+}
+
+fn additive(input: &str) -> IResult<&str, Expression> {
+    let (input, init) = multiplicative(input)?;
+
+    fold_many0(pair(alt((char('+'), char('-'))), multiplicative), init, |acc, (op, value)| {
+        let op = match op {
+            '+' => BinaryOperator::Add,
+            '-' => BinaryOperator::Subtract,
+            _ => unreachable!(), // TODO: make this an error?
+        };
+
+        Expression::Binary(op, Box::new(acc), Box::new(value))
+    })(input)
+}
+
+fn comparison(input: &str) -> IResult<&str, Expression> {
+    let (input, init) = additive(input)?;
+
+    fold_many0(
+        pair(alt((tag("=="), tag("!="), tag(">"), tag("<"), tag(">="), tag("<="))), additive),
+        init,
+        |acc, (op, value)| {
+            let op = match op {
+                "==" => BinaryOperator::Equals,
+                "!=" => BinaryOperator::NotEquals,
+                ">" => BinaryOperator::GreaterThan,
+                "<" => BinaryOperator::LessThan,
+                ">=" => BinaryOperator::GreaterThanOrEquals,
+                "<=" => BinaryOperator::LessThanOrEquals,
+                _ => unreachable!(), // TODO: make this an error?
+            };
+
+            Expression::Binary(op, Box::new(acc), Box::new(value))
+        },
+    )(input)
+}
+
+fn logical_and(input: &str) -> IResult<&str, Expression> {
+    let (input, init) = comparison(input)?;
+
+    fold_many0(preceded(tag("&&"), comparison), init, |acc, expr| {
+        Expression::Binary(BinaryOperator::LogicalAnd, Box::new(acc), Box::new(expr))
+    })(input)
+}
+
+fn logical_or(input: &str) -> IResult<&str, Expression> {
+    let (input, init) = logical_and(input)?;
+
+    fold_many0(preceded(tag("||"), logical_and), init, |acc, expr| {
+        Expression::Binary(BinaryOperator::LogicalOr, Box::new(acc), Box::new(expr))
+    })(input)
+}
+
+fn range(input: &str) -> IResult<&str, Expression> {
+    alt((
+        map(tuple((logical_or, alt((tag("..="), tag(".."))), logical_or)), |(lhs, op, rhs)| match op {
+            ".." => Expression::Range(RangeOperator::Exclusive, Box::new(lhs), Box::new(rhs)),
+            "..=" => Expression::Range(RangeOperator::Inclusive, Box::new(lhs), Box::new(rhs)),
+            _ => unreachable!(),
+        }),
+        logical_or,
+    ))(input)
+}
+
+fn coalesce(input: &str) -> IResult<&str, Expression> {
+    let (input, init) = range(input)?;
+
+    fold_many0(preceded(tag("??"), range), init, |acc, expr| {
+        Expression::Binary(BinaryOperator::Coalesce, Box::new(acc), Box::new(expr))
+    })(input)
+}
+
+fn discard(input: &str) -> IResult<&str, Expression> {
+    let (input, init) = coalesce(input)?;
+
+    fold_many0(preceded(tag(";"), coalesce), init, |acc, expr| {
+        Expression::Binary(BinaryOperator::Discard, Box::new(acc), Box::new(expr))
+    })(input)
+}
+
+fn expression(input: &str) -> IResult<&str, Expression> {
+    discard(input)
+}
+
+pub fn parse<'a>(input: &'a str) -> Result<Expression, Box<dyn std::error::Error + 'a>> {
+    let (_, result) = expression(input)?;
+
+    Ok(result)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn int_parses() -> Result<(), Box<dyn std::error::Error>> {
+        if let (_, Literal::Integer(literal)) = int_literal("1")? {
+            assert_eq!(1, literal);
+            Ok(())
+        } else {
+            panic!("Invalid type.")
+        }
+    }
+
+    #[test]
+    fn float_parses() -> Result<(), Box<dyn std::error::Error>> {
+        if let (_, Literal::Float(literal)) = float_literal("-1f")? {
+            assert_eq!(-1.0f32, literal);
+            Ok(())
+        } else {
+            panic!("Invalid type.")
+        }
+    }
+
+    #[test]
+    fn test() {
+        let output = parse(r#"[5, 6..=9, 7..8]"#).unwrap();
+        println!("{:?}", output);
+    }
+}
