cd4b580a7c84391bc1852b2095fb0cdc9e3e805f
Josh Rodgers <bytemr@gmail.com>
2020-09-04 22:21:20 -0700

    Improving scope context.

--- worp-dice/src/compiler/scope.rs
+++ worp-dice/src/compiler/scope.rs
@@ -9,22 +9,34 @@ pub(super) enum ScopeKind {
     Loop,
 }
 
+#[derive(Clone)]
 pub(super) struct ScopeContext {
     pub depth: usize,
     pub kind: ScopeKind,
+    pub exit_points: Vec<u64>,
 }
 
 pub(super) struct ScopeStack {
-    stack: Vec<ScopeKind>,
+    stack: Vec<ScopeContext>,
 }
 
 impl ScopeStack {
     pub fn new(kind: ScopeKind) -> Self {
-        Self { stack: vec![kind] }
+        Self {
+            stack: vec![ScopeContext {
+                kind,
+                depth: 0,
+                exit_points: Vec::new(),
+            }],
+        }
     }
 
     pub fn push_scope(&mut self, kind: ScopeKind) {
-        self.stack.push(kind);
+        self.stack.push(ScopeContext {
+            kind,
+            depth: self.stack.len(),
+            exit_points: Vec::new(),
+        });
     }
 
     pub fn pop_scope(&mut self) {
@@ -34,15 +46,27 @@ impl ScopeStack {
     pub fn context(&self) -> Result<ScopeContext, CompilerError> {
         self.stack
             .last()
-            .copied()
-            .map(|kind| ScopeContext {
-                kind,
-                depth: self.stack.len() - 1,
-            })
+            .cloned()
             .ok_or_else(|| CompilerError::InternalCompilerError(String::from("Scope stack underflowed.")))
     }
 
     pub fn in_context_of(&self, kind: ScopeKind) -> bool {
-        self.stack.contains(&kind)
+        for context in self.stack.iter().rev() {
+            if context.kind == kind {
+                return true;
+            }
+
+            // If the kind being searched for is a loop or block and a script, module, or function is reached, terminate the scope kind search.
+            if matches!(kind, ScopeKind::Loop | ScopeKind::Block)
+                && matches!(
+                    context.kind,
+                    ScopeKind::Script | ScopeKind::Module | ScopeKind::Function
+                )
+            {
+                return false;
+            }
+        }
+
+        return false;
     }
 }
