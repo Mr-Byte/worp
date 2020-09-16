7c0485f82e982b7a7d5b53cd80cfa4a5e31017a1
Josh Rodgers <bytemr@gmail.com>
2020-09-15 22:52:46 -0700

    Further refactoring of the scope stack to handle upvalues.

--- worp-dice/src/compiler/scope.rs
+++ worp-dice/src/compiler/scope.rs
@@ -8,6 +8,14 @@ pub struct ScopeVariable {
     pub is_captured: bool,
 }
 
+#[derive(Clone, PartialEq)]
+pub enum ScopeUpvalue {
+    /// This upvalue points to a value in the parent scope.
+    Parent { slot: usize },
+    /// This upvalue points to a value in the grandparent scope.
+    Outer { upvalue_slot: usize },
+}
+
 #[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq)]
 pub enum ScopeKind {
     Script,
@@ -35,15 +43,16 @@ pub struct ScopeContext {
     pub entry_point: Option<usize>,
     pub exit_points: Vec<usize>,
     pub variables: Vec<ScopeVariable>,
+    pub upvalues: Vec<ScopeUpvalue>,
     slot_count: usize,
 }
 
-impl ScopeContext {
-    /// Get a list of captured variables. Used while closing any upvalues from the scope.
-    pub fn captured_variables(&self) -> impl Iterator<Item = &ScopeVariable> {
-        self.variables.iter().filter(|variable| variable.is_captured)
-    }
-}
+// impl ScopeContext {
+//     /// Get a list of captured variables. Used while closing any upvalues from the scope.
+//     pub fn captured_variables(&self) -> impl Iterator<Item = &ScopeVariable> {
+//         self.variables.iter().filter(|variable| variable.is_captured)
+//     }
+// }
 
 impl Default for ScopeContext {
     fn default() -> Self {
@@ -53,6 +62,7 @@ impl Default for ScopeContext {
             entry_point: None,
             exit_points: Vec::new(),
             variables: Vec::new(),
+            upvalues: Vec::new(),
             slot_count: 0,
         }
     }
@@ -90,36 +100,6 @@ impl ScopeStack {
         self.stack
             .pop()
             .ok_or_else(|| CompilerError::InternalCompilerError(String::from("Scope stack underflowed.")))
-    }
-
-    /// Find the first scope of the specified kind.
-    /// If the specified scope is of type Loop, Block, or If and a Function, Script, or Module boundary is encountered
-    /// before the specified scope can be found, this function returns None.
-    pub fn first_of_kind(&self, kind: ScopeKind) -> Option<&ScopeContext> {
-        for context in self.stack.iter().rev() {
-            if context.kind == kind {
-                return Some(context);
-            } else if kind.is_block() && context.kind.is_terminal() {
-                return None;
-            }
-        }
-
-        None
-    }
-
-    /// Find the first scope of the specified kind as a mutable reference.
-    /// If the specified scope is of type Loop, Block, or If and a Function, Script, or Module boundary is encountered
-    /// before the specified scope can be found, this function returns None.
-    pub fn first_of_kind_mut(&mut self, kind: ScopeKind) -> Option<&mut ScopeContext> {
-        for context in self.stack.iter_mut().rev() {
-            if context.kind == kind {
-                return Some(context);
-            } else if kind.is_block() && context.kind.is_terminal() {
-                return None;
-            }
-        }
-
-        None
     }
 
     /// Determine whether or not if the stack currently contains a context of the specified scope type.
@@ -164,26 +144,25 @@ impl ScopeStack {
     /// Find the first local variable with the specified name, starting at the top of the stack and working towards the bottom.
     /// This searches each scope in reverse order of variable declarations, so that the most recently used declaration is
     /// return first.
-    pub fn local(&self, name: Symbol) -> Option<ScopeVariable> {
-        self.stack
-            .iter()
-            .rev()
-            .flat_map(|scope| scope.variables.iter().rev())
-            .find(|local| local.name == name)
-            .cloned()
+    pub fn local(&self, name: Symbol) -> Option<&ScopeVariable> {
+        for scope in self.stack.iter().rev() {
+            for variable in scope.variables.iter().rev() {
+                if variable.name == name {
+                    return Some(variable);
+                }
+            }
+
+            if scope.kind.is_terminal() {
+                return None;
+            }
+        }
+
+        return None;
     }
 
-    pub fn upvalue(&self, name: Symbol) -> Option<ScopeVariable> {
-        todo!("Implement upvalue resolution.")
-    }
-
-    fn outer(&self) -> impl Iterator<Item = &ScopeContext> {
-        // NOTE: Start from the top of the stack and skip every scope inside the current terminal scope.
-        self.stack
-            .iter()
-            .rev()
-            .skip_while(|scope| !scope.kind.is_terminal())
-            .skip(1)
+    pub fn add_upvalue(&self, upvalue: ScopeUpvalue) -> Result<usize, CompilerError> {
+        // Find the first function scope and insert this upvalue into it, if it does not already contain the upvalue.
+        Ok(0)
     }
 
     pub fn top_mut(&mut self) -> Result<&mut ScopeContext, CompilerError> {
@@ -209,5 +188,56 @@ impl ScopeStack {
             .entry_point
             .clone()
             .ok_or_else(|| CompilerError::InternalCompilerError(String::from("Not in a loop context.")))
+    }
+
+    /// Get all terminal scopes on the stack, ordered top to bottom.
+    fn terminal_scopes_mut(&mut self) -> impl Iterator<Item = &mut ScopeContext> {
+        self.stack.iter_mut().rev().filter(|scope| scope.kind.is_terminal())
+    }
+
+    /// Find the first scope of the specified kind.
+    /// If the specified scope is of type Loop, Block, or If and a Function, Script, or Module boundary is encountered
+    /// before the specified scope can be found, this function returns None.
+    fn first_of_kind(&self, kind: ScopeKind) -> Option<&ScopeContext> {
+        for context in self.stack.iter().rev() {
+            if context.kind == kind {
+                return Some(context);
+            } else if kind.is_block() && context.kind.is_terminal() {
+                return None;
+            }
+        }
+
+        None
+    }
+
+    /// Find the first scope of the specified kind as a mutable reference.
+    /// If the specified scope is of type Loop, Block, or If and a Function, Script, or Module boundary is encountered
+    /// before the specified scope can be found, this function returns None.
+    fn first_of_kind_mut(&mut self, kind: ScopeKind) -> Option<&mut ScopeContext> {
+        for context in self.stack.iter_mut().rev() {
+            if context.kind == kind {
+                return Some(context);
+            } else if kind.is_block() && context.kind.is_terminal() {
+                return None;
+            }
+        }
+
+        None
+    }
+
+    fn outer_scopes_mut(&mut self) -> impl Iterator<Item = &mut ScopeContext> {
+        // NOTE: Start from the top of the stack and skip every scope inside the current terminal scope.
+        self.stack
+            .iter_mut()
+            .rev()
+            .skip_while(|scope| !scope.kind.is_terminal())
+            .skip(1)
+    }
+
+    /// Search for a variable that exist in any scope outside the current terminal scope and return a mutable reference to it.
+    fn outer_local_mut(&mut self, name: Symbol) -> Option<&mut ScopeVariable> {
+        self.outer_scopes_mut()
+            .flat_map(|scope| scope.variables.iter_mut().rev())
+            .find(|local| local.name == name)
     }
 }
